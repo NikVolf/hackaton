4629b4a8698cf715e6c78c020bd50f454a15a536
nikvolf <nikvolf@gmail.com>
2023-02-13 17:18:22 +0300

    add unimplemented

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,18 @@ use launch_io::*;
 pub const MAX_VALUE: u64 = 10_000;
 
 #[derive(Default, Encode, Decode, TypeInfo)]
+pub struct SessionStrategy {
+    pub fuel: u32,
+    pub payload: u32,
+}
+
+#[derive(Default, Encode, Decode, TypeInfo)]
 pub struct CurrentSession {
     pub altitude: u32,
     pub weather: u32,
     pub fuel_price: u32,
-    pub payload_price: u32,
+    pub payload_value: u32,
+    pub registered: BTreeMap<ActorId, SessionStrategy>,
 }
 
 #[derive(Default, Encode, Decode, TypeInfo)]
@@ -38,29 +45,102 @@ impl LaunchSite {
             },
             0,
         )
-        .expect("Error in a reply `LaunchSite::info");
+        .expect("Error in a reply `::info");
     }
 
-    fn new_participant(&self, name: String) {
+    fn new_participant(&mut self, name: String) {
         let actor_id = msg::source();
 
-        unimplemented!()
+        if self.participants.contains_key(&actor_id) {
+            panic!("There is already participant registered with this id");
+        }
+
+        self.participants.insert(
+            actor_id,
+            Participant {
+                name: name.clone(),
+                balance: 0,
+            }
+        );
+
+        msg::reply(
+            Event::NewParticipant { id: actor_id, name },
+            0,
+        ).expect("failed to reply in ::new_participant");
     }
 
-    fn rename_participant(&self, name: String) {
+    fn rename_participant(&mut self, name: String) {
         let actor_id = msg::source();
 
-        unimplemented!()
+        if !self.participants.contains_key(&actor_id) {
+            panic!("There is no participant registered with this id");
+        }
+
+        let participant = self.participants.get_mut(&actor_id).expect("checked above that exists");
+
+        participant.name = name.clone();
+
+        msg::reply(
+            Event::ParticipantNameChange { id: actor_id, name },
+            0,
+        ).expect("failed to reply in ::rename_participant");
     }
 
-    fn new_session(&self) {
+    fn new_session(&mut self) {
 
-        unimplemented!()
+        let actor_id = msg::source();
+
+        assert_eq!(actor_id, self.owner);
+        assert!(self.current_session.is_none());
+
+        let random_weather = 100;
+        let random_fuel_price = 100;
+        let random_payload_value = 100;
+        let random_altitude = 120_000;
+
+        self.current_session = Some(CurrentSession {
+            weather: random_weather,
+            fuel_price: random_fuel_price,
+            payload_value: random_payload_value,
+            altitude: random_altitude,
+            registered: Default::default(),
+        });
+
+        msg::reply(
+            Event::NewLaunch {
+                id: 0,
+                name: "Unnamed".to_string(),
+                weather: random_weather,
+                fuel_price: random_fuel_price,
+                payload_value: random_payload_value,
+                altitude: random_altitude,
+            },
+            0,
+        ).expect("failed to reply in ::new_session");
     }
 
-    fn register_on_launch(&self, fuel_amount: u32, payload_amount: u32) {
+    fn register_on_launch(&mut self, fuel_amount: u32, payload_amount: u32) {
+        let actor_id = msg::source();
 
-        unimplemented!()
+        assert!(self.current_session.is_some());
+
+        let current_session = self.current_session.as_mut().expect("checked above that exists");
+
+        if current_session.registered.contains_key(&actor_id) {
+            // already registered
+
+            panic!("Participant already registered on the session");
+        }
+
+        current_session.registered.insert(actor_id, SessionStrategy { fuel: fuel_amount, payload: payload_amount });
+
+        msg::reply(
+            Event::LaunchRegistration {
+                id: 0,
+                participant: actor_id,
+            },
+            0,
+        ).expect("failed to reply in ::new_session");
     }
 }
 
